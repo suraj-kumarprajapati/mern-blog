164b2bcbdd8d1d39f81d5f632d9d7f14ea16b9ac
Suraj Prajapati <sp0640481@gmail.com>
2024-06-05 00:48:32 +0530

    installed flowbite in my project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,13 +6,16 @@ import Projects from './pages/Projects';
 import Dashboard from './pages/Dashboard';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import Header from './components/Header';
 
 
 
 
 function App() {
   return (
+    
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { Button } from "flowbite-react";
+
+function Header() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default Header;
